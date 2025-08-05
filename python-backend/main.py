12684726e3cd4c0617ae62d7956338de54f9b8c1
Brandon Lum <brandon.lum650@gmail.com>
2025-08-05 15:56:44 -0700

    Update main.py

--- python-backend/main.py
+++ python-backend/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from telemetry import get_latest_telemetry
+from telemetry import get_latest_telemetry, get_session_id
+from telemetry_objects import Session, Lap, Telemetry_data
 import random
 
 app = FastAPI()
+
+
+sessions = {}
+
+#Session(get_session_id)
+#print(session.session_id)
 
 # Let frontend (localhost:5173) access backend (localhost:8000)
 app.add_middleware(
@@ -19,4 +26,5 @@ def telemetry():
     data = get_latest_telemetry()
     if data:
         return data
-    return {"speed": random.randint(0, 400), "throttle": round(random.uniform(0, 1), 2), "brake": round(random.uniform(0, 1), 2)}
+    return {"speed": 0.0, "throttle": 0.0, "brake": 0.0}
+    # return {"speed": random.randint(0, 400), "throttle": round(random.uniform(0, 1), 2), "brake": round(random.uniform(0, 1), 2)}
