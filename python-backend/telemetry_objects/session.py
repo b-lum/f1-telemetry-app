48ffcb45f6482172e8f06d18d3443e24a30913f4
Brandon Lum <brandon.lum650@gmail.com>
2025-08-17 11:58:51 -0700

    Update session.py

--- python-backend/telemetry_objects/session.py
+++ python-backend/telemetry_objects/session.py
@@ -6,7 +6,9 @@ class Session :
    def __init__(self, session_id):
       self.laps = {}
       self.session_id = session_id
+      self.most_recent_lap = None
 
    # takes in lap_object
    def add_lap(self, lap) :
       self.laps[lap.lap_number] = lap
+      self.most_recent_lap = lap
